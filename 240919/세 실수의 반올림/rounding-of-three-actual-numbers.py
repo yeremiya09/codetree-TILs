31aa2a1d41ea98ab5f0d463303911788a1b6d21a
yeremiya09 <kimjw0593@gmail.com>
2024-09-19 10:50:21 +0900

    Title: 세 실수의 반올림, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240919/세 실수의 반올림/rounding-of-three-actual-numbers.py
+++ 240919/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -1,12 +1,7 @@
-# 정수 a 입력
-# 두 실수 입력받기
+# 변수 선언, 입력
 a = float(input())
 b = float(input())
 c = float(input())
 
-
-
-# 소수점 세째 자리까지 출력
-print(f"{a:.3f}")
-print(f"{b:.3f}")
-print(f"{c:.3f}")
+# 출력
+print(f"{a:.3f}\n{b:.3f}\n{c:.3f}")
